8518ca11b77eb2a4c68a9bbe01fba591190a2e5b
TimRCAnderson <timothyrcanderson@gmail.com>
2017-04-06 17:41:30 -0400

    changing BrewDB API URL to refer to Nathan's Nodejs server.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-	var brewDBURL = "https://api.brewerydb.com/v2/locations?key=f763de9765b39a76ecb1f8861767928a&locality=Charlotte"
+	var brewDBURL = "https://fathomless-plains-61908.herokuapp.com/beer/locations?key=f763de9765b39a76ecb1f8861767928a&locality=Charlotte"
 
 	$.ajax({
 		method: "GET",
-		url: brewDBURL
+		url: brewDBURL,
 	}).done(function(response) {
 		console.log(response);
 	});
