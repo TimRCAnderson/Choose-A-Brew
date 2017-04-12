127d1b836e7fc4be77694d7b473bf7b891fb7858
LV Magee <lvmagee@gmail.com>
2017-04-12 01:37:48 -0400

    Html and css changes.
    Added more html and css.

--- assets/js/openLink.js
+++ assets/js/openLink.js
@@ -3,3 +3,22 @@
 		$(".w3-display-middle .shown").addClass("hidden").removeClass("shown");
 		$("#" + text).removeClass("hidden").addClass("shown");
 	}
+
+
+	// 	// Tabs
+		// function openLink(evt, linkName) {
+		// 	var i, x, tablinks;
+		// 	x = document.getElementsByClassName("myLink");
+		// 	for (i = 0; i < x.length; i++) {
+		// 		x[i].style.display = "none";
+		// 	}
+		// 	tablinks = document.getElementsByClassName("tablink");
+		// 	for (i = 0; i < x.length; i++) {
+		// 		tablinks[i].className = tablinks[i].className.replace(" w3-orange", "");
+		// 	}
+		// 	document.getElementById(linkName).style.display = "block";
+		// 	evt.currentTarget.className += " w3-orange";
+		// }
+		// // Click on the first tablink on load
+		// document.getElementsByClassName("tablink")[0].click();
+
