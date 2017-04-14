ed4cd31c11365abd777efe096d0d81011d634c25
LV Magee <lvmagee@gmail.com>
2017-04-14 19:26:35 -0400

    Commit to get updated
    Working on responsiveness in the nave bar.

--- assets/js/app.js
+++ assets/js/app.js
@@ -254,6 +254,15 @@ $(document).ready(function() {
 	}
 }
 
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
 	//TODO: add D3js bar graph for rating distribution
 	//TODO: finish search results displays
 	//TODO: pass .data() of beer name to rating submission form.
